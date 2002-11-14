dda3da66d662ffbd7378e3968d5581e1ac5257a9
jrb <>
2002-11-14 21:30:13 +0000

    First import

---
+++ src/Dll/CalibSvc_dll.cxx
@@ -0,0 +1,13 @@
+/** @file CalibSvc_dll.cpp
+
+  $Header: /nfs/slac/g/glast/ground/cvs/GlastSvc/src/Dll/GlastSvc_dll.cpp,v 1.4 2002/03/15 21:13:31 heather Exp $
+*/
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(CalibSvc)
+  //LOAD_FACTORY_ENTRIES(CalibMySQLCnvSvc)
+
+  // LOAD_FACTORY_ENTRIES(CalibDataSvc)
+
+
+  // LOAD_FACTORY_ENTRIES(CalibCnv)
