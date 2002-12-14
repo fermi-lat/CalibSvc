8934172cf3ad1bee1eef334d5dbabd21af9f5796
jrb <>
2002-12-14 00:12:18 +0000

    oops.  Will have to modify and recommit.

--- src/Dll/CalibSvc_load.cxx
+++ src/Dll/CalibSvc_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/GlastSvc/src/Dll/GlastSvc_load.cpp,v 1.20 2002/05/10 19:19:11 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/Dll/CalibSvc_load.cxx,v 1.1.1.1 2002/11/14 21:30:13 jrb Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -14,11 +14,9 @@ DECLARE_FACTORY_ENTRIES(CalibSvc) {
 
   DECLARE_SERVICE(CalibMySQLCnvSvc);
 
-  // Following don't exist yet.  besides, they will be private,
-  // invoked only by the MySQL converter, so maybe they don't
-  // have to be declared here at all.
-  //  DECLARE_SERVICE(CalibXMLCnvSvc);
-  //  DECLARE_SERVICE(CalibROOTCnvSvc);
+  // Following don't exist yet.
+  //  DECLARE_SERVICE(CalibXmlCnvSvc);
+  //  DECLARE_SERVICE(CalibRootCnvSvc);
 
   DECLARE_CONVERTER(MetadataCnv);   // to convert MetadataEntryCol. NYW.
 
