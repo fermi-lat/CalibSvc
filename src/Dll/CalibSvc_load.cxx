cbda11663d28cdfe8a1591dfed29234408fdb021
jrb <>
2002-12-14 00:15:13 +0000

    Declare xml conversion service and base converter.

--- src/Dll/CalibSvc_load.cxx
+++ src/Dll/CalibSvc_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/Dll/CalibSvc_load.cxx,v 1.1.1.1 2002/11/14 21:30:13 jrb Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/Dll/CalibSvc_load.cxx,v 1.2 2002/12/14 00:12:18 jrb Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -13,12 +13,13 @@ DECLARE_FACTORY_ENTRIES(CalibSvc) {
 
 
   DECLARE_SERVICE(CalibMySQLCnvSvc);
+  DECLARE_SERVICE(CalibXmlCnvSvc);
+
+  DECLARE_CONVERTER(XmlBaseCnv);  
 
   // Following don't exist yet.
-  //  DECLARE_SERVICE(CalibXmlCnvSvc);
   //  DECLARE_SERVICE(CalibRootCnvSvc);
-
-  DECLARE_CONVERTER(MetadataCnv);   // to convert MetadataEntryCol. NYW.
+  // DECLARE_CONVERTER(XmlTest1Cnv);
 
 
 } 
