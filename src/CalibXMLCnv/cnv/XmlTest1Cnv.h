f0e77ccc26dfdf502316d5a152f5a2fa58b84f6a
jrb <>
2002-12-21 00:59:14 +0000

    First concrete converter and supporting cast

--- src/CalibXMLCnv/cnv/XmlTest1Cnv.h
+++ src/CalibXMLCnv/cnv/XmlTest1Cnv.h
@@ -1,18 +1,34 @@
-// $Header:    $
+// $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/CalibXMLCnv/cnv/XmlTest1Cnv.h,v 1.1 2002/12/14 00:10:51 jrb Exp $
 #ifndef CalibData_XmlTest1Cnv_h
 #define CalibData_XmlTest1Cnv_h
 
 /** @class XmlTest1  
 
   Converter from xml to very simple calibration data-like class to be 
-  used for testing 
-  calibration infrastructure
+  used for testing calibration infrastructure
 
   @author J. Bogart
 */
-class XmlTest1Cnv : XmlBaseCnv {
+#include "XmlBaseCnv.h"
+#include <dom/DOM_Element.hpp>
 
+class XmlTest1Cnv : public XmlBaseCnv {
 
+  /// Friend needed for instantiation
+  friend class CnvFactory<XmlTest1Cnv>;
+public:
+  const CLID& objType() const;
+  static const CLID& classID();
+protected:
+
+  XmlTest1Cnv(ISvcLocator* svcs);
+
+  virtual ~XmlTest1Cnv() {}       // most likely nothing to do 
+
+  virtual StatusCode i_createObj(const DOM_Element& element,
+                                 DataObject*& refpObject);
+
+  
 };
 
 
