064ffdb74942b0afc974244402e2c885aacca28e
jrb <>
2002-12-21 00:58:38 +0000

    New getValidInterval service

--- src/CalibMySQLCnv/CalibMySQLCnvSvc.cxx
+++ src/CalibMySQLCnv/CalibMySQLCnvSvc.cxx
@@ -1,4 +1,4 @@
-//$Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/CalibMySQLCnv/CalibMySQLCnvSvc.cxx,v 1.3 2002/11/22 18:13:52 jrb Exp $
+//$Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/CalibMySQLCnv/CalibMySQLCnvSvc.cxx,v 1.4 2002/12/14 00:09:22 jrb Exp $
 #include <string>
 //#include <stdio.h>   // ???
 #include <cstdio>
@@ -710,3 +710,26 @@ calibUtil::Metadata* CalibMySQLCnvSvc::getMeta( ) {
 }
 
 
+StatusCode CalibMySQLCnvSvc::getValidInterval(unsigned int& serNo,
+                                              ITime*& vStart, ITime*& vEnd) {
+  using calibUtil::Metadata;
+  using CalibData::CalibTime;
+
+  if (vStart != 0) delete vStart;
+  if (vEnd != 0) delete vEnd;
+  facilities::Timestamp* since;
+  facilities::Timestamp* till;
+  Metadata::eRet ret = m_meta->getInterval(serNo, since, till);
+
+  StatusCode status = StatusCode::FAILURE;
+
+  if (ret == Metadata::RETOk) {
+    vStart = new CalibTime(*since);
+    vEnd = new CalibTime(*till);
+    status = StatusCode::SUCCESS;
+  }
+
+  delete since;
+  delete till;
+  return status;
+}
