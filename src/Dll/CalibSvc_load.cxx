78c525e5d6534177a39cacb38cb030b1a395a832
jrb <>
2003-01-29 18:32:50 +0000

    Add converter XmlBadStripsCnv for CalibData::BadStrips

--- src/Dll/CalibSvc_load.cxx
+++ src/Dll/CalibSvc_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/Dll/CalibSvc_load.cxx,v 1.3 2002/12/14 00:15:13 jrb Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/Dll/CalibSvc_load.cxx,v 1.4 2003/01/09 22:34:02 jrb Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -15,8 +15,8 @@ DECLARE_FACTORY_ENTRIES(CalibSvc) {
   DECLARE_SERVICE(CalibMySQLCnvSvc);
   DECLARE_SERVICE(CalibXmlCnvSvc);
 
-  //  DECLARE_CONVERTER(XmlBaseCnv);  
   DECLARE_CONVERTER(XmlTest1Cnv);
+  DECLARE_CONVERTER(XmlBadStripsCnv);
 
   // Following doesn't exist yet.
   //  DECLARE_SERVICE(CalibRootCnvSvc);
