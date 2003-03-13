b651b40dfde2aaebc972f7411135a10452777d2a
jrb <>
2003-03-13 23:01:29 +0000

    Don't forget to declare CAL converters

--- src/Dll/CalibSvc_load.cxx
+++ src/Dll/CalibSvc_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/Dll/CalibSvc_load.cxx,v 1.4 2003/01/09 22:34:02 jrb Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/Dll/CalibSvc_load.cxx,v 1.5 2003/01/29 18:32:50 jrb Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -17,6 +17,8 @@ DECLARE_FACTORY_ENTRIES(CalibSvc) {
 
   DECLARE_CONVERTER(XmlTest1Cnv);
   DECLARE_CONVERTER(XmlBadStripsCnv);
+  DECLARE_CONVERTER(XmlCalPedCnv);
+  DECLARE_CONVERTER(XmlCalGainCnv);
 
   // Following doesn't exist yet.
   //  DECLARE_SERVICE(CalibRootCnvSvc);
