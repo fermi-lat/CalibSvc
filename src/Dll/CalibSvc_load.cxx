72a2930b02301a3db82c68adb8446db3f1644fb4
jrb <>
2004-08-09 17:49:50 +0000

    Add converter for XML format tracker splits calibration

--- src/Dll/CalibSvc_load.cxx
+++ src/Dll/CalibSvc_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/Dll/CalibSvc_load.cxx,v 1.12 2004/06/11 20:49:07 jrb Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/CalibSvc/src/Dll/CalibSvc_load.cxx,v 1.13 2004/07/27 05:53:08 jrb Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -30,6 +30,7 @@ DECLARE_FACTORY_ENTRIES(CalibSvc) {
   DECLARE_CONVERTER(XmlAcdPedCnv);
   DECLARE_CONVERTER(XmlAcdGainCnv);
 
+  DECLARE_CONVERTER(XmlTkrSplitsCnv);
 
   DECLARE_SERVICE(CalibRootCnvSvc);
   DECLARE_CONVERTER(RootCalGainCnv)
