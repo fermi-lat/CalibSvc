da4b20f517dc15b559e6cfa4115fd02bbe37bc4e
ecephas <>
2008-11-05 21:15:33 +0000

    Added the following libraries:
    	     env.Tool('addLibrary', library = env['rootLibs'])
    	     env.Tool('addLibrary', library = env['rootGuiLibs'])
         	     env.Tool('LdfEventLib')

--- CalibSvcLib.py
+++ CalibSvcLib.py
@@ -1,4 +1,4 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalibSvc/CalibSvcLib.py,v 1.1 2008/07/09 21:13:41 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalibSvc'])
@@ -8,6 +8,8 @@ def generate(env, **kw):
     env.Tool('EventLib')
     env.Tool('commonRootDataLib')
     env.Tool('calibRootDataLib')
-
+    env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('addLibrary', library = env['rootGuiLibs'])
+    env.Tool('LdfEventLib')
 def exists(env):
     return  1;
