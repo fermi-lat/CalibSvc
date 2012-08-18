0824347b5505bb99170f58c5c5c4a1b11743c4b5
jrb <>
2012-08-18 00:20:34 +0000

    support no-install headers on Windows

--- CalibSvcLib.py
+++ CalibSvcLib.py
@@ -1,7 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalibSvc/CalibSvcLib.py,v 1.1 2008/07/09 21:13:41 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/CalibSvc/CalibSvcLib.py,v 1.2 2008/11/05 21:15:33 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['CalibSvc'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'CalibSvc') 
     env.Tool('CalibDataLib')
     env.Tool('xmlBaseLib')
     env.Tool('calibUtilLib')
@@ -11,5 +13,7 @@ def generate(env, **kw):
     env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['rootGuiLibs'])
     env.Tool('LdfEventLib')
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'MootSvc') 
 def exists(env):
     return  1;
